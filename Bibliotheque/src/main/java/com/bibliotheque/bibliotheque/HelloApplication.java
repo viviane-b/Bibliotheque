546028bf9ea503712e7662f687e4bf370d4fd398
Avunz <felix@zuluboom.net>
2024-04-20 11:15:15 -0400

    interface javafx init
    On peut rajouter des alignements et rendre ça beau pour Ahmed

--- Bibliotheque/src/main/java/com/bibliotheque/bibliotheque/HelloApplication.java
+++ Bibliotheque/src/main/java/com/bibliotheque/bibliotheque/HelloApplication.java
@@ -2,7 +2,15 @@ package com.bibliotheque.bibliotheque;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.sql.*;
 
@@ -13,16 +21,33 @@ import static java.lang.System.exit;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        var titr = new Text("Bibliothèque");
+        titr.setFont(Font.font(35));
+
+        Button b1 = new Button("Question 1");
+        Button b2 = new Button("Question 2");
+        Button b3 = new Button("Question 3");
+        Button b4 = new Button("Question 4");
+
+        var aonhsant = new TextArea("haawa");
+
+
+        VBox qqqq = new VBox(b1, b2, b3, b4);
+        var tooo = new HBox(qqqq, aonhsant);
+        var truc = new VBox(titr, tooo);
+        truc.setAlignment(Pos.CENTER);
+
+
+
+        Scene scene = new Scene(truc, 800, 600);
+        stage.setTitle("Bibliothèque");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //launch(args);
+        launch(args);
         exit(0);
     }
 }
