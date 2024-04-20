1fc02b375e6364799fc4b6a49f5babddfe7e75c7
viviane <vivianebinet@hotmail.com>
2024-04-20 12:51:00 -0400

    enlever choses inutiles

--- Bibliotheque/src/main/java/com/bibliotheque/bibliotheque/BiblioConnexion.java
+++ Bibliotheque/src/main/java/com/bibliotheque/bibliotheque/BiblioConnexion.java
@@ -16,7 +16,7 @@ public class BiblioConnexion {
         String username = "postgres";
         String password = "Data45wer!";
 
-        //       ScriptRunner scriptRunner = new ScriptRunner;
+
 
 
         try {
@@ -26,35 +26,10 @@ public class BiblioConnexion {
             }
 
             String setSchema = "Set search_path to bibliotheque";
-            String select1 = "Select * from livre";
-            Statement statement = connection.createStatement();
-            statement.execute(setSchema);
-            ResultSet result = statement.executeQuery(select1);
 
-            // System.out.println("\n ISBN et noms des livres:");
-            while (result.next()) {
-                String ISBN = result.getString("ISBN");
-                String titre = result.getString("titre");
-                int anneePub = result.getInt("anneePub");
-                String edition = result.getString("edition");
-                String langue = result.getString("langue");
-                String genre = result.getString("genre");
+             Statement statement = connection.createStatement();
+             statement.execute(setSchema);
 
-                //    System.out.println(ISBN + " " + titre);
-            }
-
-            // System.out.println(" \n Copies disponibles:");
-            String viewLivreDispo = "select * from LivreDispo";
-            ResultSet livreDispo = connection.createStatement().executeQuery(viewLivreDispo);
-
-            while (livreDispo.next()) {
-                String ISBN = livreDispo.getString("ISBN");
-                int copiesDispo = livreDispo.getInt("copiesDispo");
-
-                //     System.out.println(ISBN + " " + copiesDispo);
-            }
-
-            // connection.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
